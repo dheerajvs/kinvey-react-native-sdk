b720dc1b1576bad6b8e8afc32a084a655bd9d9b5
Chris Miller <chris@dialexa.com>
2017-10-04 14:44:44 -0500

    Realm

--- ios/RNKinveyReactNativeSdk.podspec
+++ ios/RNKinveyReactNativeSdk.podspec
@@ -9,14 +9,15 @@ Pod::Spec.new do |s|
   s.homepage     = ""
   s.license      = "MIT"
   # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
-  s.author             = { "author" => "author@domain.cn" }
+  s.author             = { "author" => "chris@dialexa.com" }
   s.platform     = :ios, "7.0"
-  s.source       = { :git => "https://github.com/author/RNKinveyReactNativeSdk.git", :tag => "master" }
+  s.source       = { :git => "https://github.com/dialexa/kinvey-react-native-sdk.git", :tag => "Realm" }
   s.source_files  = "RNKinveyReactNativeSdk/**/*.{h,m}"
   s.requires_arc = true
 
 
   s.dependency "React"
+  s.dependency "Realm"
   #s.dependency "others"
 
 end
--- middleware/src/storage/index.js
+++ middleware/src/storage/index.js
@@ -1,10 +1,10 @@
-import { isDefined } from 'kinvey-js-sdk/dist/export';
-import { Storage as CoreStorage } from 'kinvey-js-sdk/dist/request/src/middleware/src/storage';
-import { AsyncStorageAdapter } from './asyncstorage';
+import { isDefined } from "kinvey-js-sdk/dist/export";
+import { Storage as CoreStorage } from "kinvey-js-sdk/dist/request/src/middleware/src/storage";
+import { RealmAdapter } from "./realm";
 
 export default class Storage extends CoreStorage {
   loadAdapter() {
-    return AsyncStorageAdapter.load(this.name).then(adapter => {
+    return RealmAdapter.load(this.name).then(adapter => {
       if (isDefined(adapter) === false) {
         return super.loadAdapter();
       }
