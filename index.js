1ed75673388a9f951cfcfe58c717487b57c0fc7e
Thomas Conner <thomas.conner@me.com>
2017-12-15 11:13:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-import { CacheRack, NetworkRack, MobileIdentityConnect } from 'kinvey-js-sdk';
-import * as Kinvey from 'kinvey-js-sdk';
-import { CacheMiddleware, HttpMiddleware } from './middleware';
-import Popup from './popup';
-
-// Setup racks
-CacheRack.useCacheMiddleware(new CacheMiddleware());
-NetworkRack.useHttpMiddleware(new HttpMiddleware());
-
-// Setup popup
-MobileIdentityConnect.usePopupClass(Popup);
-
-// Export
+import * as Kinvey from './bundle';
 module.exports = Kinvey;
